be84ef978a72896d117fc50a26acc53c4d2c2733
jnobles <123Jimmyn@gmail.com>
2023-01-24 03:00:55 -0500

    Removed un-needed dicts

--- gui/view.py
+++ gui/view.py
@@ -3,40 +3,39 @@ import tkinter as tk
 class BatchArchivalAssistant(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.labels = {}
         self.buttons = {}
         self.entries = {}
-        self.entries_stringVars = {}
 
-    def create_label(self, parent, name, text, row, col, rowspan=1, colspan=1):
-        label = tk.Label(parent, text=text)
-        self.labels[name] = label
-        label.grid(row=row, column=col, rowspan=rowspan, columnspan=colspan, sticky='nsew')
+    def create_label(self, parent, text, row, col, rowspan=1, colspan=1, anchor=tk.CENTER):
+        label = tk.Label(parent, text=text, anchor=anchor)
+        settings = {'row':row, 'column':col, 'rowspan':rowspan, 'columnspan':colspan,
+                    'sticky':tk.NSEW}
+        label.grid(**settings)
 
     def create_button(self, parent, name, text, row, col, rowspan=1, colspan=1):
         button = tk.Button(parent, text=text)
         self.buttons[name] = button
-        settings = {'sticky':tk.NSEW, 'padx':2, 'pady':2}
-        button.grid(row=row, column=col, rowspan=rowspan, columnspan=colspan, **settings)
+        settings = {'row':row, 'column':col, 'rowspan':rowspan, 'columnspan':colspan,
+                    'sticky':tk.NSEW, 'padx':2, 'pady':2}
+        button.grid(**settings)
 
     def create_entry(self, parent, name, row, col, rowspan=1, colspan=1):
         stringVar = tk.StringVar()
-        self.entries_stringVars[name] = stringVar
         entry = tk.Entry(parent, textvariable=stringVar)
-        self.entries[name] = entry
-
-        settings = {'sticky':tk.NSEW}
-        entry.grid(row=row, column=col, rowspan=rowspan, columnspan=colspan, **settings)
+        self.entries[name] = stringVar
+        settings = {'row':row, 'column':col, 'rowspan':rowspan, 'columnspan':colspan,
+                    'sticky':tk.NSEW}
+        entry.grid(**settings)
 
     def create_main_window(self):
         entry_frame = tk.Frame(self)
-        self.create_label(entry_frame, 'catalog', 'Catalog Number', 0, 0)
-        self.create_entry(entry_frame, 'catalog_entry', 0, 1)
+        self.create_label(entry_frame, 'Catalog Number', 0, 0, anchor=tk.E)
+        self.create_entry(entry_frame, 'catalog', 0, 1)
 
-        self.create_label(entry_frame, 'lot', 'Lot Number', 1, 0)
+        self.create_label(entry_frame, 'Lot Number', 1, 0, anchor=tk.E)
         self.create_entry(entry_frame, 'lot_entry', 1, 1)
 
-        self.create_label(entry_frame, 'year', 'Release Year', 2, 0)
+        self.create_label(entry_frame, 'Release Year', 2, 0, anchor=tk.E)
         self.create_entry(entry_frame, 'year_entry', 2, 1)
 
         grid_cols, grid_rows = entry_frame.grid_size()
